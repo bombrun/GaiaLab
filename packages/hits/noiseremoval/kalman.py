233d5b21dae7e80fe720d9583091c0011484afb8
tobyjamez <tobyswjames@gmail.com>
2018-07-06 11:12:31 +0100

    Kalman filter functional. Class design improved.

--- packages/hits/noiseremoval/kalman.py
+++ packages/hits/noiseremoval/kalman.py
@@ -57,7 +57,11 @@
  It is implemented here as a class to accept measured data and apply the Kalman filter.
  This returns a generator allowing large datasets to be used without being committed to memory twice.
  
- Current implementation supports 1-dimensional, constant data. 
+ Current implementation supports 1-dimensional data.
+
+ References:
+    kalmanfilter.net
+    Welch G, Bishop G; An Introduction to the Kalman Filter; UNC; 1994.
 
  Toby James 2018
 """
@@ -75,6 +79,7 @@ class KalmanData():
     """
     Data with Kalman filter applied.
     """
+    #-----special methods-----------------------------------------------------
     def __init__(self, data_array, save=False):
         """
         Accepts:
@@ -89,15 +94,15 @@ class KalmanData():
                 If True, saves the Kalman-filtered data to self.kalman_data on initialisation.
                 If False, doesn't. This saves memory.
         """
-        if  hasattr(data_array, "__iter__"):
+        if  hasattr(data_array, "__iter__"): # if data_array can be iterated over, iterate over it and add each element
 
-            if isinstance(data_array, pd.DataFrame):
+            if isinstance(data_array, pd.DataFrame): # this is a more hefty task, delegated to self._from_pandas()
                 self._from_pandas(data_array)
 
-            elif all(isinstance(data_point, (int, float)) for data_point in data_array):
+            elif all(isinstance(data_point, (int, float)) for data_point in data_array): # check that each element is a real number
                 self._data = array('d', data_array)
 
-            else:
+            else: # isolate the issue with the given array and raise a TypeError
                 for data_point in data_array:
                     if not isinstance(data_point, (int, float)):
                         bad_type = type(data_point)
@@ -107,31 +112,34 @@ class KalmanData():
         else:
             raise(TypeError("Data type %r is not supported. Please supply an iterable of numerical values." % type(data_array)))
         
-        self._kalman_data = KalmanData._kalman(self._data)
+        self._kalman_data = KalmanData._kalman(self._data) # set up a generator within
         
-        if save:
+        if save: # saves kalman data as a list rather than as a generator. more resource heavy but useful nonetheless
             self.kalman_data = list(KalmanData._kalman(self._data))
 
-    def __len__(self):
-        """Delegate to __len__ of array.array."""
+    def __len__(self):#delegate to __len__ of array.array.
+        """Length of the data."""
         return len(self._data)
 
-    def __repr__(self):
-        """No benefit in showing all values for large objects. Especially since the most useful data in this class is generated on the fly."""
+    def __repr__(self): #No benefit in showing all values for large objects. Especially since the most useful data in this class is generated on the fly.
+        """Representation of the data."""
         return "Kalman data object, length: %r\n[%r ... %r]" % (len(self), self._data[0], self.data[-1])
     
 
     def __getitem__(self, index):
-        """Apply Kalman filter to selected regions."""
-        self._kalman_data = KalmanData._kalman(self.data[index])
-        return self._data[index]
+        """
+        Apply Kalman filter to selected regions.
+        Returns a list rather than a generator.
+        """
+        return list(KalmanData._kalman(self._data[index]))
+        
     
     # Comparison operators:
     #-------------------------------------
     def checkdatatype(func): # decorator to automatically return false for non-matching datatypes
         """Wrap verify as a decorator."""
         def verify(self, other):
-            """Automatically returns False for comparison of two non-matching datatypes."""
+            """Automatically returns False for comparison of two non-matching datatypes. Compares matching datatypes as expected."""
             if isinstance(other,KalmanData):
                 return(func(self, other))
             else:
@@ -158,28 +166,31 @@ class KalmanData():
         return self._data != other._data
     #--------------------------------------
 
-    def __add__(self, other):
+    def __add__(self, other): #addition is implemented elementwise
         """Allows addition of scalars and (elementwise) arrays of equal length to self."""
-        if isinstance(other, KalmanData):
+        if isinstance(other, KalmanData):#this is probably never going to be used but it would be bizarre for a class to refuse to combine with another instance of itself
             if len(self) == len(other):
                 return KalmanData([a + b for a, b in zip(self._data, other._data)])
             else:
                 raise(ValueError("Unable to broadcast together operands of shape %r and %r." % (len(self),len(other))))
-        elif hasattr(other, "__iter__") and not isinstance(other, str):
+
+        elif hasattr(other, "__iter__") and not isinstance(other, str): #this is largely more likely: adding another array of the same length to the data
             if len(self) == len(other):
                 return KalmanData([a + b for a, b in zip(self._data, other)])
             else:
                 raise(ValueError("Unable to broadcast together operands of shape %r and %r." % (len(self),len(other))))
-        elif isinstance(other, (float, int)):
+        
+        elif isinstance(other, (float, int)): # if adding a constant to the data, add it to every element
             return KalmanData([a + other for a in self._data])
+        
         else:
             raise(TypeError("Unable to broadcast together operands of type %r and %r." % (type(self), type(other))))
     
-    def __mul__(self, other):
+    def __mul__(self, other): # multiply by scalars and elementwise by arrays
         """Allows multiplication by scalars and elementwise by arrays of equal length to self."""
         if isinstance(other, (int, float)):
             return KalmanData([a * other for a in self._data])
-        elif hasattr(other, "__iter__") and len(other) == len(self) and not isinstance(other, str):
+        elif hasattr(other, "__iter__") and len(other) == len(self) and not isinstance(other, str): # all of these are necessary for elementwise multiplication to make sense
             return KalmanData([a * b for a,b in zip(self, other)])
         else:
             raise(TypeError("Unable to multiply types %r and %r." % (type(self), type(other))))
@@ -193,7 +204,7 @@ class KalmanData():
         else:
             raise(TypeError("Unable to broadcast together operands of type %r and %r." % (type(self), type(other))))
     
-    def __truediv__(self, other):
+    def __truediv__(self, other): # analogous to multiplication
         """Allows division by scalars and elementwise by arrays of equal length to self.""" 
         if isinstance(other, (float, int)):
             return KalmanData([a/other for a in self._data])
@@ -207,85 +218,49 @@ class KalmanData():
     def __call__(self): # allow easy iteration over generated data.
         """Returns a generator for the Kalman filtered data."""
         return self._kalman_data
+    #-------------------------------------------------------------------------
 
-    def _from_pandas(self, df): # I wanted this to be a separate function called by __init__. Not sure why, but I think it is sufficiently unique to justify existing in its own right.
-        """Create KalmanData object from pandas dataframe."""
-        if 'rate' and 'w1_rate' in df.columns:
-            data_array = df['rate'] - df['w1_rate']
+    #private methods and variables (so far as python allows)------------------
+    _indices = None
+    _obmt = None
+    _q = None
+    _r = None
+
+    def _from_pandas(self, df): # I wanted this to be a separate function called by __init__. I think it is sufficiently unique to justify existing in its own right.
+        """
+        Create KalmanData object from pandas dataframe.
+
+        Since the obmt column of the pandas dataframe is not necessarily sorted, this is harder than creating from raw arrays as 
+        information on the order of the obmt and the indices needs to be stored for the algorithm to work and return a dataframe 
+        of the same shape as expected.
+        """
+        #obmt is not always increasing therefore if it is given, sort the data according to obmt.
+        if 'obmt' in df.columns:# saves obmt and indices to lists. this obviously takes up potentially unnecessary memory
+            sorted_df = df.sort_values('obmt')
+            self._indices = sorted_df.index.values.tolist()
+            self._obmt = sorted_df['obmt'].tolist()
+        else:
+            sorted_df = df
+
+        #if rate and windowed rate are given, good - it will behave as expected
+        if 'rate' and 'w1_rate' in sorted_df.columns:
+            data_array = (sorted_df['rate'] - sorted_df['w1_rate']).tolist()
             self._data = data_array
-        elif 'rate' in df.columns:
-            data_array = df['rate']
+        #if just the rate is given it will attempt to work with that but depending on the origin of the data this will not necessarily work as well
+        elif 'rate' in sorted_df.columns:
+            data_array = sorted_df['rate'].tolist()
             self._data = data_array
+        #if rate is not given the dataset is unusable
         else:
             raise(ValueError("The dataframe used does not have the correct columns to use this method. Please manually prepare the data and initialise the class the normal way."))
-    
-    def to_pandas(self, obmt=None, columns=None, w1_rate=None):
-        """
-        Accepts:
-            
-            Nothing.
 
-        Returns a pandas dataframe of the Kalman filtered data.
-
-        Kwargs:
-
-            obmt(array-like, default=None):
-                Time series to match the data to. If none is given, defaults to an array of 1 second intervals.
-
-            w1_rate(array-like, default=None):
-                Windowed data. If none is given, it is created from the calculated data.
-
-        Returns:
-            
-            A pandas dataframe of shape:
-                
-                    obmt    rate    w1_rate
-                1.  float   float   float
-
-        """
-        if obmt is None:
-            obmt = range(len(self))
-        if w1_rate is not None:
-            rate = self.kalman_data - w1_rate
-            df = pd.DataFrame(data=dict(obmt = obmt,
-                                          rate = rate,
-                                          w1_rate = w1_rate))
-        else:
-            rate = self.kalman_data
-            df = pd.DataFrame(data=dict(obmt = obmt,
-                                          rate = rate))
-            df['w1_rate'] = df['rate'].copy().rolling(window=3600, min_periods=0).mean()
-        return df
-        
-
-    q = None
-    r = None
-    
-    def save(self):
-        """Saves the Kalman filtered data to a variable - self.kalman_data."""
-        self.kalman_data = list(self._kalman_data)
-
-    def reset(self):
-        """Resets the generator."""
-        self._kalman_data = KalmanData._kalman(self._data)
-    
-    def tweak_q(self, q):
-        """Change the variance of the noise on the measurable value."""
-        self.q = q
-        self.reset()
-
-    def tweak_r(self, r):
-        """Change the variance of the noise on the measured value."""
-        self.r = r
-        self.reset()
-    
     @staticmethod
-    def _var(data_array, samples):
+    def _var(data_array, samples): #really trivial function, gets an estimate for the noise by observing the first samples
         """Variance of the data."""
         return np.var(data_array[0:samples])
 
     @staticmethod
-    def _kalman(data_array, r=r, q=q, samples=50):
+    def _kalman(data_array, r=_r, q=_q, samples=50):
         """
         Accepts:
             
@@ -296,6 +271,8 @@ class KalmanData():
         Performs the Kalman filter algorithm on the calculated data.
 
         Is a generator rather than a function. self._kalman_data is the generator applied to self._data.
+
+        This data is accessible by calling the class as self().
 
         Kwargs:
             
@@ -330,3 +307,86 @@ class KalmanData():
             p = (1-K)*p
             yield x
             i += 1
+    #-------------------------------------------------------------------------
+
+    #public methods-----------------------------------------------------------
+    def reset(self):
+        """Resets the generator."""
+        self._kalman_data = KalmanData._kalman(self._data)
+    
+    def save(self):
+        """Saves the Kalman filtered data to a variable - self.kalman_data."""
+        self.reset() # reset first to ensure the generator starts from the first datapoint
+        self.kalman_data = list(self._kalman_data)
+
+    def tweak_q(self, q):
+        """Change the variance of the noise on the measurable value."""
+        self._q = q
+        self.reset()
+
+    def tweak_r(self, r):
+        """Change the variance of the noise on the measured value."""
+        self._r = r
+        self.reset()
+    
+    
+    def to_pandas(self, obmt=None, columns=None, w1_rate=None):
+        """
+        Accepts:
+            
+            Nothing.
+
+        Returns a pandas dataframe of the Kalman filtered data.
+
+        Kwargs:
+
+            obmt(array-like, default=None):
+                Time series to match the data to. If none is given, defaults to an array of 1 second intervals.
+
+            w1_rate(array-like, default=None):
+                Windowed data. If none is given, it is created from the calculated data.
+
+        Returns:
+            
+            A pandas dataframe of shape:
+                
+                    obmt    rate    w1_rate
+                1.  float   float   float
+
+        """
+        self.save() # to return a dataframe, the data needs to exist first 
+        
+        if self._obmt is not None:
+        #if obmt is already saved, then use this as the obmt column
+            obmt=self._obmt
+        elif obmt is None and self._obmt is None:
+        #if it isn't, and there is no obmt given, just use an array
+            obmt = np.linspace(0,len(self._data)-1, len(self._data))
+       
+        if w1_rate is not None:
+        #if a w1_rate is given, calculate the rate from that
+                rate = self.kalman_data + w1_rate
+        else:
+        #if not, just use the kalman data as the rate data, set w1_data as None
+                rate = self.kalman_data
+
+        if self._indices is not None:
+        #if indices are saved, use these to index
+            indices = self._indices
+        else:
+        #if not, use an array
+            indices = np.linspace(0,len(self._data)-1, len(self._data))
+
+        #generate dataframe
+        sorted_df = pd.DataFrame(index=indices, data=dict(obmt = obmt,
+                                                          rate = rate,
+                                                          w1_rate = w1_rate))
+        #if the initial data was created from a pandas dataframe, the indices will now be out of order.
+        #sort the dataframe by index to allow the data to be more comparable with the original dataframe.
+        df = sorted_df.sort_index()
+
+        #if w1_rate was set as None, create new windowed data
+        if df['w1_rate'].values[0] is None:
+            df['w1_rate'] = df['rate'].copy().rolling(window=3600, min_periods=0).mean()
+
+        return df
