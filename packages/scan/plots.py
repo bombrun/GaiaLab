fb0f77e4b1724d96561b558a4f6c5c2482ba7231
Val <Vargar>
2018-07-23 14:55:35 +0200

    Update for new code structure

--- packages/scan/plots.py
+++ packages/scan/plots.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-from NSL import*
-
 
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
+#def plot_att(scan):
 
 
-
-def plot_attitude(satellite, ti, tf, dt): #change this function, maybe better use storing list since we aredy have it?
+def plot_attitude(satellite, ti, tf, dt):
     '''
     Args
     ______
@@ -59,7 +56,8 @@ def plot_attitude(satellite, ti, tf, dt): #change this function, maybe better us
     plt.rcParams.update({'font.size': 22})
     
     plt.show()
-   
+
+
 def plot_longlat(satellite, dt, n):
     
     satellite.reset()
@@ -79,7 +77,8 @@ def plot_longlat(satellite, dt, n):
     plt.rcParams.update({'font.size': 22})
     plt.title('Revolving scanning')
     plt.show()
-        
+
+
 def plot_xi(satellite, dt, n):
 
     satellite.Reset()
@@ -87,11 +86,11 @@ def plot_xi(satellite, dt, n):
     times = np.arange(0, n, dt)
     for i in times:
         satellite.Update(dt)
-        ecliptic_quat = rotation_quaternion(np.array([1,0,0]),-satellite.epsilon)
+        ecliptic_quat = rotation_quaternion(np.array([1, 0, 0]), -satellite.epsilon)
         z_ecliptic = quaternion_to_vector(ecliptic_quat*vector_to_quaternion(satellite.z_)*ecliptic_quat.conjugate())
-        s_rot_quat = rotation_quaternion(np.array([0,0,1]), satellite.l)
-        s_ecliptic = quaternion_to_vector(s_rot_quat*Quaternion(0,1,0,0)*s_rot_quat.conjugate())
-        angle = np.arccos(np.dot(z_ecliptic,s_ecliptic))
+        s_rot_quat = rotation_quaternion(np.array([0, 0, 1]), satellite.l)
+        s_ecliptic = quaternion_to_vector(s_rot_quat*Quaternion(0, 1, 0, 0)*s_rot_quat.conjugate())
+        angle = np.arccos(np.dot(z_ecliptic, s_ecliptic))
         
         angle_list.append(np.degrees(angle))
     
@@ -134,31 +133,24 @@ def plot_3DX(satellite, dt, n):
     ax.set_ylim(-1,1)
     ax.set_zlim(-1,1)
     plt.show() 
-    
-def plot_3DZ(satellite, dt, n, frame = None):
-    #frame allows a quaternion rotation to be applied to the z_ vector before being plotted, e.g.
-    # rotation_quaternion(np.array([1,0,0]),-gaia.epsilon) will move from the lmn frame to the ecliptic plane.
-    if frame == None:   #frame allows a quaternion rotation to be applied to the z_ vector before being plotted,
-        # e.g. rotation_quaternion(np.array([1,0,0]),-gaia.epsilon) will move from the lmn frame to the ecliptic plane.
-        frame = Quaternion(1,0,0,0)
+
+
+def plot_3DZ(satellite, dt, n):
         
-    satellite.Reset()
+    satellite.reset()
     z_list = []
     for i in np.arange(n/dt):
         satellite.Update(dt)
         z_list.append(quaternion_to_vector(frame*vector_to_quaternion(satellite.z_)*frame.conjugate()))
     
-    z_listx = [i[0] for i in z_list]
-    z_listy = [i[1] for i in z_list]
-    z_listz = [i[2] for i in z_list]
-    
-    
-    mpl.rcParams['legend.fontsize'] = 10
+    z_list_x = [i[0] for i in z_list]
+    z_list_y = [i[1] for i in z_list]
+    z_list_z = [i[2] for i in z_list]
     
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    ax.plot(z_listx, z_listy, z_listz,'--', label='Z vector rotation')
+    ax.plot(z_list_x, z_list_y, z_list_z,'--', label='Z vector rotation')
     ax.legend()
     ax.set_xlabel('l')
     ax.set_ylabel('m')
@@ -169,25 +161,24 @@ def plot_3DZ(satellite, dt, n, frame = None):
     ax.set_zlim(-1,1)
     
     plt.show()         
-     
+
+
 def plot_3DW(satellite, dt, n):
-    satellite.Reset()
+
+    satellite.reset()
     w_list = []
     for i in np.arange(n/dt):
         satellite.Update(dt)
         w_list.append(satellite.w_)
     
-    w_listx = [i[0] for i in w_list]
-    w_listy = [i[1] for i in w_list]
-    w_listz = [i[2] for i in w_list]
-    
-    
-    mpl.rcParams['legend.fontsize'] = 10
-    
+    w_list_x = [i[0] for i in w_list]
+    w_list_y = [i[1] for i in w_list]
+    w_list_z = [i[2] for i in w_list]
+
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
-    ax.plot(w_listx, w_listy, w_listz,'--', label='W: inertial rotation vector')
+    ax.plot(w_list_x, w_list_y, w_list_z,'--', label='W: inertial rotation vector')
     ax.legend()
     ax.set_xlabel('l')
     ax.set_ylabel('m')
@@ -195,40 +186,23 @@ def plot_3DW(satellite, dt, n):
     
     plt.show() 
 
+
 def plot_observations(scanner, sky):
-    x = [i[0] for i in scanner.starspositions]
-    y = [i[1] for i in scanner.starspositions]
-    z = [i[2] for i in scanner.starspositions]
+
+    x = [i[0] for i in scanner.stars_positions]
+    y = [i[1] for i in scanner.stars_positions]
+    z = [i[2] for i in scanner.stars_positions]
     
     xstar = [i.coor[0] for i in sky.elements]
     ystar = [i.coor[1] for i in sky.elements]
     zstar = [i.coor[2] for i in sky.elements]
-    
-    #mpl.rcParams['legend.fontsize'] = 18
-    
-    # 3D PLOT
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
 
-    ax.plot(x, y, z,'ro', label='Observations')
-    ax.plot(xstar, ystar, zstar, 'b*')
-    ax.legend()
-    ax.set_xlabel('l')
-    ax.set_ylabel('m')
-    ax.set_zlabel('n')
-    
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
-    
-    # 2D PLOT
-    fig, ((ax1, ax2,  ax3)) = plt.subplots(1,3)
+    fig, (ax1, ax2,  ax3) = plt.subplots(1, 3)
     fig.subplots_adjust(left=0.2, wspace=0.6)
     
     ax1.plot(x, y,'ro')
-    ax1.plot(xstar, ystar, 'b*', ms = 10)
+    ax1.plot(xstar, ystar, 'b*', ms=10)
     ax1.set(title='XY PLANE')
-    
 
     ax2.plot(y, z, 'ko')
     ax2.plot(ystar, zstar, 'b*', ms = 10)
@@ -238,15 +212,13 @@ def plot_observations(scanner, sky):
     ax3.plot(xstar, zstar, 'b*', ms = 10)
     ax3.set(title='XZ PLANE')
 
-    
-    #plt.rcParams.update({'font.size': 22})
-    
     plt.show()
-            
-def PlotDiff(satellite, sky):
+
+
+def plot_diff(satellite, sky):
     diff_list = []
     for star in sky.elements:
-        for idx, obj in enumerate(satellite.storinglist):
+        for obj in satellite.storinglist:
             diff_ = np.abs(obj[8] - star.coor)
             diff_list.append(mag(diff_))
     plt.figure()
