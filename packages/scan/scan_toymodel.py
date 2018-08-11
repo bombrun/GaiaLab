aa032ae770e83c166adf2556d1eb01525b3b7e5f
mdelvallevaro <Vargar>
2018-08-11 21:18:21 +0200

    fine_scan working

--- packages/scan/scan_toymodel.py
+++ packages/scan/scan_toymodel.py
@@ -318,7 +318,7 @@ class Scanner:
         stars_positions (list of arrays): positions calculated from obs_times of transits using satellite's attitude.
     """
 
-    def __init__(self, coarse_angle= np.radians(10)):
+    def __init__(self, coarse_angle= np.radians(5)):
         self.coarse_angle = coarse_angle
 
         #create storage
@@ -332,7 +332,7 @@ class Scanner:
         self.obs_times.clear()
         self.times_deep_scan.clear()
 
-    def coarse_scan(self, att, source, ti=0, tf=365*5, step=1):
+    def coarse_scan(self, att, source, ti=0, tf=365*5, step=1/24):
         """
         Scans sky with a dot product technique to get rough times of observation.
         :return: None
@@ -346,14 +346,28 @@ class Scanner:
 
         print('Star crossing field of view %i times' %(len(self.times_deep_scan)))
 
-    def find_solution(self, att, source):
+    def fine_scan(self, att, source):
+
         def f(t):
             to_star_unit = source.topocentric_function(att)(t) / np.linalg.norm(source.topocentric_function(att)(t))
-            return to_star_unit - att.func_x_axis_lmn(t)
-        values = []
+            diff_vector = to_star_unit - att.func_x_axis_lmn(t)
+            return diff_vector
+        all_roots = []
         for t in self.times_deep_scan:
-            values.append(f(t))
-        return values
+            t_times= np.linspace(t - 1 / 24, t + 1 / 24)
+            roots = root(f, t_times)
+            all_roots.append(roots)
+        return all_roots
+
+def root(f, range):
+    roots = []
+    for idx, t in enumerate(range):
+        if np.abs(f(t)[0]) < 0.01:
+            if np.abs(f(t)[1]) < 0.01:
+                if np.abs(f(t)[2]) < 0.01:
+                    roots.append(range[idx])
+    return roots
+
 
 def run():
     """
@@ -363,13 +377,12 @@ def run():
     """
     start_time = time.time()
     vega = Source("vega", 279.2333, 38.78, 128.91, 201.03, 286.23, -13.9)
-    scan = Scanner(0,0,0)
+    scan = Scanner()
     gaia = Attitude()
-    scan.coarse_scan(gaia, vega, 0, 365*5, 0.5)
-    scan.find_solution(gaia, vega)
+    scan.coarse_scan(gaia, vega)
+    all_roots = scan.fine_scan(gaia, vega)
 
     seconds = time.time() - start_time
     
     print('seconds:', seconds)
-    
-    return gaia, vega, scan
+    return gaia, vega, scan, all_roots
