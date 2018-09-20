8ed5eb0327e03783f0a5d72cc4510488ec295df6
zampa <zluca.lucky@gmail.com>
2018-09-20 16:18:19 +0200

    added file to store constants

---
+++ GaiaLab/scan/analytic_scanner/constants.py
@@ -0,0 +1,9 @@
+# # Constant storage file
+#
+# File containing the constants that will be used in the other files. This will
+# allow to avoid "magic numbers" in the code and also to easily change these
+# constants if we later need them more or less precises
+#
+# Luca Zampieri 2018
+
+year = 365 # [days]
