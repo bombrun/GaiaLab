42fcc06ce6d97024e7907b76c660de3a4bf92d34
zampa <zluca.lucky@gmail.com>
2018-09-21 16:35:06 +0200

    udated

--- GaiaLab/scan/analytic_scanner/constants.py
+++ GaiaLab/scan/analytic_scanner/constants.py
@@ -9,7 +9,7 @@
 import numpy as np
 
 days_per_year = 365  # [days]
-rad_per_mas = 2*np.pi/(1000*360*3600)  # [rediants] radiants per milli-arcsec
+rad_per_mas = 2*np.pi/(1000*360*3600)  # [radiants] radiants per milli-arcsec
 km_per_pc = 3.24078e-14  # [km ]kilometers per parsec
-sec_per_day = 3600*24 # [sec] seconds per day 
+sec_per_day = 3600*24  # [sec] seconds per day
 AU_per_pc = 4.8481705933824e-6  # [au] austronomical unit per parsec
