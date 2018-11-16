34e81368019642f2e08eed4b2486f9e851190a34
zampa <zluca.lucky@gmail.com>
2018-11-16 11:21:23 +0100

    added working error function

--- GaiaLab/scan/analytic_scanner/scanner.py
+++ GaiaLab/scan/analytic_scanner/scanner.py
@@ -2,7 +2,7 @@
 Scanner class implementation in Python
 
 
-@author: mdelvallevaro
+@author: LucaZampieri
 """
 
 # # Imports
@@ -17,6 +17,35 @@ from quaternion import Quaternion
 from satellite import Satellite
 from source import Source
 import helpers
+from agis_functions import *
+
+
+# fonctions used in the loop
+def phi_angle(sat, source, t):
+    Cu_unit = source.unit_topocentric_function(sat, t)
+    Su = ft.lmn_to_xyz(sat.func_attitude(t), Cu_unit)
+    """
+    Su_xy = [Su[0], Su[1], 0]  # projection on the xy-plane
+    vector, angle = helpers.get_rotation_vector_and_angle(v1=Su_xy, v2=[1, 0, 0])
+    angle = angle % (2*np.pi)
+    if angle > np.pi:
+        angle = - np.pi + (angle - np.pi)  # set [0, 2pi] to [-pi, pi]
+    return angle"""
+    Su_x, Su_y, Su_z = Su[:]
+    phi = np.arctan2(Su_y, Su_x)
+    return phi
+
+
+def eta_error(t, sat, source):
+    Cu_unit = source.unit_topocentric_function(sat, t)
+    vector_error_xyz = ft.lmn_to_xyz(sat.func_attitude(t), Cu_unit) - np.array([1, 0, 0])
+    return vector_error_xyz[1]  # WARNING: approx accurate only for small eta
+
+
+def vector_error(sat, source, t):
+    Cu_unit = source.unit_topocentric_function(sat, t)
+    vector_error_xyz = ft.lmn_to_xyz(sat.func_attitude(t), Cu_unit) - np.array([1, 0, 0])
+    return vector_error_xyz
 
 
 class Scanner:
@@ -37,13 +66,19 @@ class Scanner:
         # Products
         self.obs_times = []
         self.root_messages = []
+        self.eta_scanned = []
+        self.zeta_scanned = []
 
-    def reset_memory(self):
+    def reset(self, verbose=False):
         """
         :action: empty all attribute lists from scanner before beginning new scanning period.
         """
         self.obs_times.clear()
         self.root_messages.clear()
+        self.eta_scanned.clear()
+        self.zeta_scanned.clear()
+        if verbose:
+            print('Cleared variables!')
 
     # Scan function
     def scan(self, sat, source, ti=0, tf=5*const.days_per_year):
@@ -55,35 +90,22 @@ class Scanner:
         ideas to optimize: create array with all errors and go pick them as needed
         """
         print('Starting scan with time from {} to {} days'.format(ti, tf))
-        self.reset_memory()
-        time_step = 1/24
+        self.reset()
 
-        # fonctions used in the loop
-        def eta_got(t):
-            Cu_unit = source.unit_topocentric_function(sat, t)
-            vector, angle = helpers.get_rotation_vector_and_angle(v1=Cu_unit, v2=sat.func_x_axis_lmn(t))
-            angle = angle % (2*np.pi)
-            if angle > np.pi:
-                angle = - np.pi + (angle - np.pi)  # set [0, 2pi] to [-pi, pi]
-            return angle
-
-        def eta_error(t):
-            Cu_unit = source.unit_topocentric_function(sat, t)
-            vector_error_xyz = ft.lmn_to_xyz(sat.func_attitude(t), Cu_unit) - np.array([1, 0, 0])
-            return vector_error_xyz[1]  # WARNING: approx accurate only for small eta
-
-        def vector_error(t):
-            Cu_unit = source.unit_topocentric_function(sat, t)
-            vector_error_xyz = ft.lmn_to_xyz(sat.func_attitude(t), Cu_unit) - np.array([1, 0, 0])
-            return vector_error_xyz
+        revolutions_per_day = sat.wz/(2*np.pi)
+        time_of_revolution = 1/revolutions_per_day
+        time_step = time_of_revolution/3
 
         def violated_contraints(t_a, t_b):
             bool_violated_constraints = False
-            if np.abs(vector_error(t_a)[2] + vector_error(t_b)[2]) > np.radians(0.5):
+            # checks zeta constraint
+            if np.abs(vector_error(sat, source, t_a)[2] + vector_error(sat, source, t_b)[2]) > np.radians(0.5):
                 bool_violated_constraints = True
-            if (np.abs(eta_got(t_a)) > np.pi/2) & (np.abs(eta_got(t_b)) > np.pi/2):
+            # check about angles
+            if (np.abs(phi_angle(sat, source, t_a)) > np.pi/2) & (np.abs(phi_angle(sat, source, t_b)) > np.pi/2):
                 bool_violated_constraints = True
-            if eta_error(t_a)*eta_error(t_b) >= 0:  # check if f changes sign in [a,b]
+            # check if f changes sign in [a,b]
+            if eta_error(t_a, sat, source)*eta_error(t_b, sat, source) >= 0:
                 bool_violated_constraints = True
             return bool_violated_constraints
         # Looping
@@ -92,15 +114,13 @@ class Scanner:
             # Check constraints
             t0 = time.time()
             if violated_contraints(t, t+time_step):
-                time_elapsed = time.time()-t0
-                measured_time += time_elapsed
-                # print('time for constraints t:', t, 'is', time_elapsed)
+                measured_time += time.time()-t0
                 continue
             time_elapsed = time.time()-t0
             measured_time += time_elapsed
             print('time for constraints t:', t, 'is', time_elapsed)
             t0 = time.time()
-            x0, r = optimize.brentq(f=eta_error, a=t, b=t+time_step, args=(),
+            x0, r = optimize.brentq(f=eta_error, a=t, b=t+time_step, args=(sat, source),
                                     xtol=2e-20, rtol=8.881784197001252e-16,
                                     maxiter=100, full_output=True, disp=True)
             time_elapsed = time.time()-t0
@@ -110,10 +130,19 @@ class Scanner:
         print('Total measured time:', measured_time)
         # End of function
 
+    def compute_angles_eta_zeta(self, sat, source):
+        for t in self.obs_times:
+            Cu_unit = source.unit_topocentric_function(sat, t)
+            Su = ft.lmn_to_xyz(sat.func_attitude(t), Cu_unit)
+            eta, zeta = compute_field_angles(Su)
+            self.eta_scanned.append(eta)
+            self.zeta_scanned.append(zeta)
 
-
-
-
+    def scanner_error(self):
+        if not self.eta_scanned:
+            raise ValueError('No scanned angles in the scanner! Please scan' +
+                             ' and compute the angles')
+        return np.mean(self.eta_scanned)
 
 
 # End of file
